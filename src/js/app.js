e58e699f5d599b98646b997f5a7b1e4e16fd93a0
student <student@localhost>
2020-05-08 23:59:13 +0300

    LocalStorage added

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,40 @@
 let meny = document.querySelector('.meny');
 let menylink = document.querySelector('.burger-meny');
+let textarea = document.getElementById('textform');
+let input = document.querySelectorAll('input');
+
 menylink.addEventListener(
         'click',
         ()=>{
             meny.classList.toggle('burger-meny-active');
         }
     );
+
+textarea.value = localStorage.getItem('text');
+textarea.addEventListener(
+    'change',
+    function () {
+        localStorage.setItem('text', textarea.value)
+    }
+);
+
+inputArray = Array.from(input);
+
+inputArray[0].value = localStorage.getItem('input');
+inputArray[0].addEventListener(
+    'change',
+    function () {
+        localStorage.setItem('input', inputArray[0].value)
+    }
+);
+
+
+inputArray[1].value = localStorage.getItem('input1');
+inputArray[1].addEventListener(
+    'change',
+    function () {
+        localStorage.setItem('input1', inputArray[1].value)
+    }
+);
+
+
